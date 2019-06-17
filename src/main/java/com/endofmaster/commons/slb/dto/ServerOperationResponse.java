417253546b5e86706e41f7090837831da110dd56
EndOfMaster <843698826>
2019-06-17 19:16:27 +0800

    还原版本

--- src/main/java/com/endofmaster/commons/slb/dto/ServerOperationResponse.java
+++
@@ -1,9 +0,0 @@
-package com.endofmaster.commons.slb.dto;
-
-import com.endofmaster.commons.slb.SlbResponse;
-
-/**
- * @author ZM.Wang
- */
-public class ServerOperationResponse extends SlbResponse {
-}
