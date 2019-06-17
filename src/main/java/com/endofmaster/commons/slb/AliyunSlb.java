0a8eb45d4e7a2f571d04b3c8d8d6d0193bca6e69
EndOfMaster <843698826>
2019-06-17 19:13:13 +0800

    还原版本

--- src/main/java/com/endofmaster/commons/slb/AliyunSlb.java
+++ src/main/java/com/endofmaster/commons/slb/AliyunSlb.java
@@ -1,8 +1,78 @@
 package com.endofmaster.commons.slb;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.slb.model.v20140515.AddBackendServersResponse;
+import com.aliyuncs.slb.model.v20140515.CreateLoadBalancerHTTPListenerResponse;
+import com.aliyuncs.slb.model.v20140515.CreateLoadBalancerHTTPSListenerResponse;
+import com.aliyuncs.slb.model.v20140515.CreateLoadBalancerTCPListenerResponse;
+import com.aliyuncs.slb.model.v20140515.DeleteLoadBalancerListenerRequest;
+import com.aliyuncs.slb.model.v20140515.DeleteLoadBalancerListenerResponse;
+import com.aliyuncs.slb.model.v20140515.StartLoadBalancerListenerRequest;
+import com.aliyuncs.slb.model.v20140515.StartLoadBalancerListenerResponse;
+import com.endofmaster.commons.slb.dto.CreateHttpListenerRequest;
+import com.endofmaster.commons.slb.dto.CreateHttpsListenerRequest;
+import com.endofmaster.commons.slb.dto.CreateTcpListenerRequest;
+import com.endofmaster.commons.slb.dto.ServersOperationRequest;
+
 /**
  * @author ZM.Wang
  */
 public class AliyunSlb {
 
+    private final String regionId;
+    private final String accessKeyId;
+    private final String accessKeySecret;
+    private final IAcsClient client;
+
+    public AliyunSlb(String regionId, String accessKeyId, String accessKeySecret) {
+        this.regionId = regionId;
+        this.accessKeyId = accessKeyId;
+        this.accessKeySecret = accessKeySecret;
+        this.client = new DefaultAcsClient(DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret));
+    }
+
+    public CreateLoadBalancerHTTPListenerResponse addHttpListener(CreateHttpListenerRequest request) throws ClientException {
+        return client.getAcsResponse(request.getAliyunReq());
+    }
+
+    public CreateLoadBalancerHTTPSListenerResponse addHttpsListener(CreateHttpsListenerRequest request) throws ClientException {
+        return client.getAcsResponse(request.getAliyunHttpsReq());
+    }
+
+    public CreateLoadBalancerTCPListenerResponse addTcpListener(CreateTcpListenerRequest request) throws ClientException {
+        return client.getAcsResponse(request.getAliyunReq());
+    }
+
+    public StartLoadBalancerListenerResponse startListener(String slbId, int serverPort) throws ClientException {
+        StartLoadBalancerListenerRequest request = new StartLoadBalancerListenerRequest();
+        request.setLoadBalancerId(slbId);
+        request.setListenerPort(serverPort);
+        return client.getAcsResponse(request);
+    }
+
+    public DeleteLoadBalancerListenerResponse delListener(String slbId, int serverPort) throws ClientException {
+        DeleteLoadBalancerListenerRequest request = new DeleteLoadBalancerListenerRequest();
+        request.setLoadBalancerId(slbId);
+        request.setListenerPort(serverPort);
+        return client.getAcsResponse(request);
+    }
+
+    public AddBackendServersResponse addServer(ServersOperationRequest request) throws ClientException {
+        return client.getAcsResponse(request.getAddReq());
+    }
+
+    public String getRegionId() {
+        return regionId;
+    }
+
+    public String getAccessKeyId() {
+        return accessKeyId;
+    }
+
+    public String getAccessKeySecret() {
+        return accessKeySecret;
+    }
 }
--- src/test/java/com/endofmaster/commons/slb/AliyunSlbTest.java
+++ src/test/java/com/endofmaster/commons/slb/AliyunSlbTest.java
@@ -1,22 +1,51 @@
 package com.endofmaster.commons.slb;
 
-import com.endofmaster.commons.slb.dto.AddBackendServerRequest;
+import com.aliyuncs.exceptions.ClientException;
 import com.endofmaster.commons.slb.dto.BackendServer;
-import com.endofmaster.commons.slb.dto.ServerOperationResponse;
+import com.endofmaster.commons.slb.dto.CreateHttpListenerRequest;
+import com.endofmaster.commons.slb.dto.CreateHttpsListenerRequest;
+import com.endofmaster.commons.slb.dto.CreateTcpListenerRequest;
+import com.endofmaster.commons.slb.dto.ServersOperationRequest;
 import org.junit.Test;
 
-import java.util.Collections;
 
 public class AliyunSlbTest {
 
     private final static String SLB_ID = "lb-2ze22ot1jj79xoz049aph";
-
-    private final SlbClient client = new SlbClient("VAZSGZwX16yoLtbU", "qi5jrXwQ7VYzgMD1BMSjBl0gFG9COo");
+    private final AliyunSlb aliyunSlb = new AliyunSlb("cn-beijing", "VAZSGZwX16yoLtbU", "**************");
 
     @Test
-    public void addServer() {
-        AddBackendServerRequest request = new AddBackendServerRequest(Collections.singletonList(
-                new BackendServer("i-2zebslw1q067n115dip1", "100", "ecs")), SLB_ID, "cn-beijing");
-        ServerOperationResponse response = client.execute(request);
+    public void createHttpListener() throws ClientException {
+        CreateHttpListenerRequest request = new CreateHttpListenerRequest(SLB_ID, 80, 8080);
+        aliyunSlb.addHttpListener(request.healthCheck("/test"));
+        aliyunSlb.startListener(SLB_ID, 80);
+    }
+
+    @Test
+    public void createHttpsListener() throws ClientException {
+        CreateHttpsListenerRequest request = new CreateHttpsListenerRequest(SLB_ID, 443, 8080);
+        request.healthCheck("/test");
+        request.setCertId(Constant.WEJUAI_SLB_CERT_ID);
+        aliyunSlb.addHttpsListener(request);
+        aliyunSlb.startListener(SLB_ID, 443);
+    }
+
+    @Test
+    public void createTcpListener() throws ClientException {
+        CreateTcpListenerRequest request = new CreateTcpListenerRequest(SLB_ID, 80, 8080);
+        aliyunSlb.addTcpListener(request);
+        aliyunSlb.startListener(SLB_ID, 80);
+    }
+
+    @Test
+    public void delListener() throws ClientException {
+        aliyunSlb.delListener(SLB_ID, 80);
+    }
+
+    @Test
+    public void addServer() throws ClientException {
+        ServersOperationRequest request = new ServersOperationRequest(SLB_ID);
+        request.addServer(new BackendServer("i-2zebslw1q067n115dip1", "100", "ecs"));
+        aliyunSlb.addServer(request);
     }
 }
